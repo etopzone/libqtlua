29cbe24222860a5548b441934d16a2c790cbd113
diaxen <diaxen@935d6458-678b-4b3c-8a87-d3c7175e91dd>
2012-01-08 03:26:37 +0000

    updated examples

--- trunk/libqtlua/examples/cpp/proxy/arrayproxy_string.cc
+++ trunk/libqtlua/examples/cpp/proxy/arrayproxy_string.cc
@@ -18,46 +18,48 @@
 
 */
 
-#include <QtLua/UserObject>
+// This example show how to use an ArrayProxy object to access a C array
+// object from lua script.
 
-								/* anchor 1 */
-class Test : public QtLua::UserObject<Test>
-{
-  QTLUA_USEROBJECT(Test);
-
-  QTLUA_PROPERTY(int, _value);
-
-public:
-  Test(int value)
-    : _value(value)
-  {
-  }
-
-};
-
-QTLUA_PROPERTIES_TABLE(Test,
-  QTLUA_PROPERTY_ENTRY(Test, "value", _value)
-);
-/* anchor end */
+#include <iostream>
 
 #include <QtLua/State>
-#include <QtLua/Function>
+#include <QtLua/String>
+#include <QtLua/ArrayProxy>
 
 int main()
 {
   try {
+							/* anchor 1 */
+    QtLua::String array[4];
+
+    // Array proxy which provides access to our array from lua.
+    QtLua::ArrayProxy<QtLua::String> proxy(array, 4);
+							/* anchor 2 */
 
     QtLua::State state;
     state.openlib(QtLua::QtLuaLib);
 
-    state["foo"] = QTLUA_REFNEW(Test, 21);
-    state["bar"] = QTLUA_REFNEW(Test, 42);
+    // Declare a lua global variable using our array proxy
+    state["array"] = proxy;
 
-    state.exec_statements("for key, value in each(foo) do print(key, value) end");
+    // Set a value in array
+    array[0] = "foo";
+
+    // Read/Write array from lua using the proxy object
+    state.exec_statements("array[2] = array[1]..\"bar\" ");
+
+							/* anchor 3 */
+    // Read back value in array modified from lua script
+    std::cout << array[1].constData() << std::endl;
+
+    // Iterate through array from lua script
+    state.exec_statements("for key, value in each(array) do print(key, value) end");
+							/* anchor end */
+
   } catch (QtLua::String &e) {
     std::cerr << e.constData() << std::endl;
   }
 
-
-  return 0;
 }
+
